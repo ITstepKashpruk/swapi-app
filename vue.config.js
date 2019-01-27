83bb5b47c591538a4e37ef2c2f8f1e9f759f5f1a
Tatiana <itstepkashpruk@gmail.com>
2019-01-27 19:11:13 +0200

    swapi-app

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/swapi-app2/dist/' : '/',
+	publicPath: process.env.NODE_ENV === 'production' ? '/swapi-app/dist/' : '/',
 };
